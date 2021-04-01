23b2cc9257a269d1057b045a2cd11d03a893a3cf
Adictya <aditya.26101@gmail.com>
2021-04-01 18:52:19 +0530

    publish working

--- src/pages/StreamView.jsx
+++ src/pages/StreamView.jsx
@@ -2,26 +2,30 @@ import classes from './StreamView.module.css'
 import AgoraPlayerContainer from '../components/AgoraPlayerContainer'
 import AgoraGameContainer from '../components/AgoraGameContainer'
 import {
+  // createMicrophoneAndCameraTracks,
   createClient
 } from 'agora-rtc-react'
 import {useEffect} from 'react'
 
 const config = {mode: 'live', codec: 'vp8'}
-const appId = ''
+const appId = '1e6816ded05149088f32daa1c0d19456'
+
+// const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
 
 const useClient = createClient(config)
 
 const StreamView = (props) => {
   const client = useClient()
-  const cname = props.channelName | "defaultStream"
-  const uname = props.channelName | "default"
+  const cname = props.channelName? props.channelName : "defaultStream"
+  const uname = props.userName? props.userName : "defaultUser"
   useEffect(()=>client.setClientRole('host'),[])
   useEffect(() => {
     let init = async (name) => {
       console.log("init",name);
       await client.join(appId,name,null,uname)
     }
-  })
+    init(cname)
+  },[cname,client,uname])
   return (
     <>
       <div className={classes.streamContainer}>
