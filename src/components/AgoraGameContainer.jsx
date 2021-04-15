c4b49764e7cf053dd8ea00a66b28a43879b65015
Adictya <aditya.26101@gmail.com>
2021-04-15 16:44:20 +0530

    formatting

--- src/components/AgoraGameContainer.jsx
+++ src/components/AgoraGameContainer.jsx
@@ -1,12 +1,17 @@
-import { AgoraVideoPlayer} from "agora-rtc-react";
+import { AgoraVideoPlayer } from "agora-rtc-react";
 
 const AgoraGameContainer = (props) => {
   const { ready, tracks } = props;
 
-
   return (
-      ready && <AgoraVideoPlayer className="AgoraVideoPlayer" videoTrack={tracks} style={{height: '100%', width: '100%'}}/>
-  )
-}
+    ready && (
+      <AgoraVideoPlayer
+        className="AgoraVideoPlayer"
+        videoTrack={tracks}
+        style={{ height: "100%", width: "100%" }}
+      />
+    )
+  );
+};
 
-export default AgoraGameContainer
+export default AgoraGameContainer;
--- src/components/AgoraPlayerContainer.jsx
+++ src/components/AgoraPlayerContainer.jsx
@@ -1,11 +1,17 @@
-import { AgoraVideoPlayer} from "agora-rtc-react";
+import { AgoraVideoPlayer } from "agora-rtc-react";
 
 const AgoraPlayerContainer = (props) => {
   const { ready, tracks } = props;
 
-    return (
-      ready && <AgoraVideoPlayer className="AgoraVideoPlayer" videoTrack={tracks[1]} style={{height: '100%', width: '100%'}}/>
+  return (
+    ready && (
+      <AgoraVideoPlayer
+        className="AgoraVideoPlayer"
+        videoTrack={tracks[1]}
+        style={{ height: "100%", width: "100%" }}
+      />
     )
-}
+  );
+};
 
-export default AgoraPlayerContainer
+export default AgoraPlayerContainer;
